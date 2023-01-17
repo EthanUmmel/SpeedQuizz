34a3cd527895800bd1820e2f0b548353155ef251
ummeeth <Ethan.Ummel@divtec.ch>
2023-01-17 11:45:15 +0100

    Réalisation du design du speedQuizz

--- app/src/main/java/com/ummeeth/speedquizz/activity_game.java
+++ app/src/main/java/com/ummeeth/speedquizz/activity_game.java
@@ -2,13 +2,56 @@ package com.ummeeth.speedquizz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.os.Bundle;
 
 public class activity_game extends AppCompatActivity {
+
+    Runnable questionRunnable = null;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
     }
+
+    private void startQuestionIterative() {
+        handler = new Handler();
+        questionRunnable = new Runnable() {
+            @Override
+            public void run() {
+                //Détecter si c'est la dernière question
+                //if (){
+                    //Do last question process
+                //}else{
+                    //Do question iterative
+                //    handler.postDelayed(this, 2000);
+                //}
+            }
+        };
+
+        handler.postDelayed(questionRunnable, 1000);
+
+    }
+
+
+
+
+    private void startCountDownTimer() {
+        new CountDownTimer(6000, 1000){
+            public void onTick(long millisUntilFinished){
+                //XXX.setText(""+millisUntilFinished / 1000);
+            };
+            public void onFinish(){
+                //XXX.setText(getText(R.string.XXX));
+            };
+
+        }.start();
+    }
+
+
+
+
 }
